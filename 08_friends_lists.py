02b2dec7a7cd524180dbf341711c231415a8cbe4
rachamallapranavi <rachamallapranavi@gmail.com>
2026-08-28 15:00:24 +0530

    Create 08_friends_lists.py

---
+++ 08_friends_lists.py
@@ -0,0 +1,15 @@
+# Pranavi's Level 2 - Lists
+# Date: Today
+
+# My friends list
+friends = ["Suma", "chathurya", "manogna"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+
+# Adding new friend
+friends.append("kavya")
+
+# Saying Hi to all friends
+for name in friends:
+    print("Hi", name)
